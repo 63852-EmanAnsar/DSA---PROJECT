26f1140462d50041ef4da414f360d9ea4908e8ce
63852-EmanAnsar <63852@students.riphah.edu.pk>
2025-11-15 21:31:06 +0500

    Create WordScramble.cpp

---
+++ WordScramble.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+	string word;
+    string hint;
+    Node* next;
+    
+    Node(string wo, string hi){
+    word=wo;
+    hint=hi;
+    next=NULL;
+	}
+};
+
+int main(){
+	
+}
